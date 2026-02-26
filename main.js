07358867d72001d2df9bfa47d05cfa4e16cf21a2
jd2693889-ctrl <jd2693889@gmail.com>
2026-02-26 16:32:15 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+// emailService.js
+
+(function () {
+  let isLoaded = false;
+
+  function loadScript() {
+    return new Promise((resolve, reject) => {
+      if (isLoaded) return resolve();
+
+      const script = document.createElement("script");
+      script.src =
+        "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
+
+      script.onload = function () {
+        isLoaded = true;
+        window.emailjs.init("GlpCP9x-1");
+        resolve();
+      };
+
+      script.onerror = reject;
+
+      document.body.appendChild(script);
+    });
+  }
+
+  async function send(data) {
+    await loadScript();
+
+    return window.emailjs.send(
+      "service_1",
+      "template_1",
+      data
+    );
+  }
+
+  // expose global function
+  window.EmailService = {
+    send: send
+  };
+})();
